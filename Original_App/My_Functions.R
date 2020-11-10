72b4f4e99a83bb941bf4be4bda4aae823cc1614d
jakepscott <jakepscott16@gmail.com>
2020-11-09 23:25:21 -0500

    Cleaned code and made more thorough comments for the Original_App code

--- Original_App/My_Functions.R
+++ Original_App/My_Functions.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(gt)
 library(usmap)
-#library(albersusa)
+#library(albersusa) technically used this to get the map data, but Shiny hates this package:(
 library(qpcR)
 library(scales)
 library(ggtext)
@@ -16,6 +16,7 @@ source("Economist_Functions.R")
 
 
 # General Gauge Function --------------------------------------------------
+#All this code comes from this thread: https://stackoverflow.com/questions/50042214/fill-a-polygon-with-gradient-scale-in-r
 
 gg.gauge <- function(pos, breaks = c(0, 33, 66, 100), determinent, Biden_win_prob, trump_win_prob) {
   # get time
@@ -36,7 +37,6 @@ gg.gauge <- function(pos, breaks = c(0, 33, 66, 100), determinent, Biden_win_pro
   #caption for gauge plot
   cap <- paste("Biden win probability, last updated at", t_string, "EST")
   
-  # colors from https://flatuicolors.com/palette/defo
   ggplot() + 
     geom_segment(data = get.poly(breaks[1],breaks[4]), 
                  aes(x = x, y = y, xend = xend, yend = yend, color = xend),size=2) +
@@ -48,6 +48,7 @@ gg.gauge <- function(pos, breaks = c(0, 33, 66, 100), determinent, Biden_win_pro
     coord_fixed()+
     theme_bw()+
     labs(
+      #Making title dependent on win probability
       title = case_when(Biden_win_prob==100~"<span style='color: #2E74C0'>**Biden**</span> has won the 2020 election",
                         Biden_win_prob<100 & Biden_win_prob>=90~"Overall, <span style='color: #2E74C0'>**Biden**</span> is clearly favored to win",
                         Biden_win_prob<90 & Biden_win_prob>=75~"Overall, <span style='color: #2E74C0'>**Biden**</span> is favored to win",
@@ -74,6 +75,7 @@ gg.gauge <- function(pos, breaks = c(0, 33, 66, 100), determinent, Biden_win_pro
 
 
 # Biden Gauge Function ----------------------------------------------------------
+#Basically a wrapper for the generage gauge function
 Biden_Gauge_Function <- function(results){
   biden_win_prob <- results$nation$biden_win_prob
   trump_win_prob <- 100-results$nation$biden_win_prob
@@ -82,15 +84,18 @@ Biden_Gauge_Function <- function(results){
 
 # Table Function ----------------------------------------------------------
 table_function <- function(results, called) {
+  #Getting state name to state abbreviation key table
   states <- cbind(state.name,state.abb) %>% as_tibble() 
-  
+  #Making the called data have a common column with the state name state abb key
   called <- called %>% rename("state.abb"=State) %>% rename("Called"=State_Winner)
   
+  #Making empty string for non-called states, so there can be a blank entry in the table for them
   table_data <- results$states %>% 
     as_tibble() %>% 
     left_join(called) %>% 
     mutate(Called=ifelse(is.na(Called),"",Called)) 
-  
+
+  #Making "called" an ordered factor so I can arrange the table using i
   table_data$Called <- factor(table_data$Called,
                               levels=c("Biden",
                                        "",
@@ -98,7 +103,7 @@ table_function <- function(results, called) {
                               labels=c("Biden",
                                        "",
                                        "Trump"))
-  
+  #Making a full table of state names, win probs, and called statuses
   table_data <- table_data %>% 
     left_join(states) %>%
     mutate(state.name=ifelse(is.na(state.name),
@@ -108,10 +113,13 @@ table_function <- function(results, called) {
     mutate(value=round(value,1)) %>% 
     dplyr::select(state.name,value, Called)
   
+  #Splitting the above full table in half, so the table can fit on the screen. Need to do some temp renaming to make 
+  #it play nice with tidyverse functions
   top <- table_data %>% head(nrow(table_data)/2)
   bottom<- table_data %>% tail(nrow(table_data)/2) %>% rename("state"=state.name,"percent"=value,"Called2"=Called)
   table_data <- cbind(top,bottom)
   
+  #Actually making the table itself
   table_data %>% 
     mutate(value = value / 100,
            percent = percent / 100) %>%
@@ -119,6 +127,7 @@ table_function <- function(results, called) {
     gt() %>% 
     #Setting title
     tab_header(title = "Biden Win Probability by State") %>% 
+    #Formatting relevant columns as percents
     fmt_percent(columns = c(2,5), decimals = 1) %>%
     cols_align(align = "center", columns = ) %>%
     tab_options(
@@ -132,18 +141,21 @@ table_function <- function(results, called) {
       heading.align = "center",
       table.font.size = 11
     ) %>% 
+    #Manually labeling columns
     cols_label(state.name="",Called="Winner",value="Win Probability",
                state="","percent"="Win Probability", Called2="Winner") %>% 
     cols_align(align = "left",
                columns = c(1,3,4,6)) %>%
     cols_align(align = "center",
                columns = c(2,5)) %>%
+    #Setting color for win prob columns
     data_color(
       columns = vars(value,percent),
       colors = scales::col_numeric(
         palette = c("#CB454A", "#2E74C0"),
         domain = c(0,1)
       )) %>% 
+    #Setting color for called status columns
     data_color(
       columns = vars(Called,Called2),
       colors = c("#2E74C0","White","#CB454A")
@@ -152,87 +164,44 @@ table_function <- function(results, called) {
 
 # Map Function ------------------------------------------------------------
 map_function <- function(results) {
-states <- cbind(state.name,state.abb) %>% as_tibble()
-USA_Map <- read_rds("USA_Map.rds") %>% sf::st_as_sf()
-st_crs(USA_Map) <- "laea"
-map_data <- results$states %>% 
-  as_tibble() %>% 
-  left_join(states) %>%
-  mutate(state.name=ifelse(is.na(state.name),
-                           state.abb,
-                           state.name)) %>% 
-  mutate(value=round(value,1),
-         state=tolower(state.name)) %>% 
-  mutate(label=paste0("Biden Win Probability: ", value, "%")) %>% 
-  mutate(state.name=ifelse(state.name=="DC","District of Columbia",state.name)) %>% 
-  dplyr::select(name=state.name,value) %>% 
-  mutate(label=paste0("Biden Win Probability: ", value, "%"))
-
-plot <- left_join(USA_Map,map_data) %>% 
-  ggplot() +
-  geom_sf(aes(fill=value,text=label)) +
-  scale_fill_gradient(low = "#CB454A",
-                      high = "#2E74C0")+
-  labs(title="Where Biden is favored to <span style='color: #2E74C0'>win</span> versus <span style='color: #CB454A'>lose</span> ") +
-  theme_minimal(base_size = 12) +
-  theme(title=element_markdown(size=rel(1),face="bold", hjust = .5),
-        axis.title = element_blank(),
-        axis.text = element_blank(),
-        panel.grid = element_blank(),
-        legend.position = "none")
+  #Getting state name state abb key table
+  states <- cbind(state.name,state.abb) %>% as_tibble()
+  #Because Shiny hates albersusa package, I downloaded the shapefile as an rds, and here I am manually loading it back in
+  #And bringing it back to an sf object
+  USA_Map <- read_rds("USA_Map.rds") %>% sf::st_as_sf()
+  #Manually resetting projection to LAEA, otherwise Shiny yells at me. Not sure why
+  st_crs(USA_Map) <- "laea"
   
+  map_data <- results$states %>% 
+    as_tibble() %>% 
+    left_join(states) %>%
+    mutate(state.name=ifelse(is.na(state.name),
+                             state.abb,
+                             state.name)) %>% 
+    mutate(value=round(value,1),
+           state=tolower(state.name)) %>% 
+    mutate(label=paste0("Biden Win Probability: ", value, "%")) %>% 
+    mutate(state.name=ifelse(state.name=="DC","District of Columbia",state.name)) %>% 
+    dplyr::select(name=state.name,value) %>% 
+    #This will be the label that shows up when you hover over a state on the app
+    mutate(label=paste0("Biden Win Probability: ", value, "%"))
   
-  ggplotly(plot,tooltip = "text") %>% 
-    style(hoveron="fills") 
+  plot <- left_join(USA_Map,map_data) %>% 
+    ggplot() +
+    #The text aesthetic doesn't exist here in reality, but is a placeholder used later by ggplotly to generate the 
+    #label that appears when a user hovers over a state
+    geom_sf(aes(fill=value,text=label)) +
+    scale_fill_gradient(low = "#CB454A",
+                        high = "#2E74C0")+
+    labs(title="Where Biden is favored to <span style='color: #2E74C0'>win</span> versus <span style='color: #CB454A'>lose</span> ") +
+    theme_minimal(base_size = 12) +
+    theme(title=element_markdown(size=rel(1),face="bold", hjust = .5),
+          axis.title = element_blank(),
+          axis.text = element_blank(),
+          panel.grid = element_blank(),
+          legend.position = "none")
+    
+    
+    ggplotly(plot,tooltip = "text") %>% 
+      style(hoveron="fills") #Makes it hover on the fill rather than the lines
 }
-
-# # Line Graph Function -----------------------------------------------------
-# results <- update_prob_for_viz(trump_states = "FL",
-#                                biden_states = c("PA","MI","WI"))
-# Biden_win_prob <- results$nation$biden_win_prob
-# trump_win_prob <- 100-results$nation$biden_win_prob
-# 
-# # prop_over_time <- tibble(biden_win_prob=results$nation$biden_win_prob,
-# #                          trump_win_prob=100-biden_win_prob,
-# #                          timestamp=Sys.time())
-# 
-# prop_over_time <- prop_over_time %>% rbind(tibble(biden_win_prob=results$nation$biden_win_prob,
-#                                                   trump_win_prob=100-biden_win_prob,
-#                                                   timestamp=Sys.time()))
-# 
-# #Getting image table
-# icons <- tibble(Candidate=c("biden_win_prob","trump_win_prob"),
-#                 icon=c("pngs/bidentransparent.png","pngs/trumptransparent.png"))
-# 
-# 
-# prop_over_time %>% pivot_longer(cols=biden_win_prob:trump_win_prob,
-#                                 names_to="Candidate",
-#                                 values_to="Win_Prob") %>%
-#   group_by(Candidate) %>%
-#   mutate(current_prob=tail(Win_Prob,1)) %>%
-#   left_join(icons) %>%
-#   ggplot() +
-#   geom_line(aes(x=timestamp,y=Win_Prob,color=Candidate),size=2) +
-#   geom_image(aes(x=max(timestamp),y=current_prob,image=icon), asp = 2, size = .04) +
-#   scale_color_manual(values = c("#2E74C0","#CB454A")) +
-#   scale_x_datetime(breaks = date_breaks("1 hour"), labels = date_format("%a %I:%M",
-#                                                                         tz = "EST")) +
-#   coord_cartesian(xlim = c(as.POSIXct("2020-11-02 18:00:00 EST"),
-#                            as.POSIXct("2020-11-03 4:00:00 EST"))) +
-#   labs(title=case_when(Biden_win_prob>=55~paste0("<span style='color: #2E74C0'>**Biden**</span> has a ", round(Biden_win_prob,1),"% chance to win the election"),
-# 
-#                        Biden_win_prob<55 & Biden_win_prob>=45~paste0("It's a Tossup, <span style='color: #2E74C0'>**Biden**</span> has a ", round(Biden_win_prob,1), "% chance to win"),
-# 
-#                        Biden_win_prob<45~paste0("<span style='color: #CB454A'>**Trump**</span> has a ", round(trump_win_prob,1),"% chance to win the election"))) +
-#   theme_minimal(base_size = 12, base_family = "Roboto Condensed") +
-#   theme(panel.grid = element_blank(),
-#         plot.title = element_markdown(face = "bold", size = rel(3)),
-#         plot.subtitle = element_text(face = "plain", size = rel(1.5), color = "grey70"),
-#         axis.text.y = element_text(size=rel(1)),
-#         axis.text.x = element_text(size=rel(.75)),
-#         legend.position = "none",
-#         axis.title = element_blank(),
-#         plot.title.position = "plot")
-
-
-
--- Original_App/app.R
+++ Original_App/app.R
@@ -1,13 +1,13 @@
+# Loading Libs ------------------------------------------------------------
 library(shiny)
 library(plotly)
 library(gsheet)
 library(googlesheets4)
 
-#setwd("C:/Users/Jake Scott/Desktop/R Projects/Election_Night_Economist_Model")
+# Loading Functions -------------------------------------------------------
 source("My_Functions.R")
 
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
   # avoid greying out plot while recalculating
   tags$style(type="text/css",
@@ -17,7 +17,7 @@ ui <- fluidPage(
   ),
   
   sidebarLayout(
-    # Sidebar with a slider input
+    #Adding sidebar with table in it, cheeky graphical trick that did the job
     sidebarPanel(width = 4,
                  tags$style(".well {background-color:[white];}"),
                  gt_output("table")
@@ -39,23 +39,30 @@ ui <- fluidPage(
 
 # Server Side -------------------------------------------------------------
 server <- function(input, output) {
-  #Reative Values
+  #Reative Values; reading in the data
   Data <- reactive({
+    #This tells it to rerun every 10 seconds
     invalidateLater(10000)
+    #This automatically reads in the data on which states have been called, every 10 seconds. I update the sheet manually
     sheet <- gsheet2tbl("https://docs.google.com/spreadsheets/d/1ZPvodyP0oWZe6PqKs2UfjuXIj1c8OXZqI6FyeH-fOl4/edit?usp=sharing")
+    #Getting which states were won by who
     Biden_Wins <- sheet %>% filter(State_Winner=="Biden") %>% pull(State)
     Trump_Wins <- sheet %>% filter(State_Winner=="Trump") %>% pull(State)
+    #Plugging in which states were won by who into a function that recalculates win prob with the new info using the  
+    #Economist model
     results <- update_prob_for_viz(biden_states = Biden_Wins,trump_states = Trump_Wins)
     results
   })
   
   Called <-  reactive({
     invalidateLater(10000)
+    #Just read in the called states data every 10 seconds
     gsheet2tbl("https://docs.google.com/spreadsheets/d/1ZPvodyP0oWZe6PqKs2UfjuXIj1c8OXZqI6FyeH-fOl4/edit?usp=sharing")
   })
   
   Line_Chart <- reactive({
     invalidateLater(10000)
+    #Read in the line chart data every 10 seconds
     gsheet2tbl("https://docs.google.com/spreadsheets/d/1yQo7vkCQ7F6aMyelJvkKS32RfTGYtA8X1z8s9OKX4sU/edit#gid=1482552590") %>% 
       mutate(timestamp=as.POSIXct(timestamp))
   })
@@ -80,30 +87,31 @@ server <- function(input, output) {
 
 # Generating Line Plot ----------------------------------------------------
   output$lineplot <- renderPlot({
+    #Getting properly formatted timestamp
     t <- Sys.time() %>% as.POSIXct(format(),tz="EST")
     t_string <- strftime(t,"%I:%M %p",tz = "EST")
     
-    #Getting image table
+    #Getting image table, with icons for Biden and Trump
     icons <- tibble(Candidate=c("biden_win_prob","trump_win_prob"),
                     icon=c("pngs/bidentransparent.png","pngs/trumptransparent.png"))
 
-
+    #Getting in tidy form for graphing
     Line_Chart() %>% pivot_longer(cols=biden_win_prob:trump_win_prob,
                                     names_to="Candidate",
                                     values_to="Win_Prob") %>%
       group_by(Candidate) %>%
       mutate(current_prob=tail(Win_Prob,1)) %>%
+      #joining with icon table
       left_join(icons) %>%
       ggplot() +
       geom_line(aes(x=timestamp,y=Win_Prob,color=Candidate),size=2) +
       geom_image(aes(x=max(timestamp),y=current_prob,image=icon), asp =3, size = .03) +
       scale_color_manual(values = c("#2E74C0","#CB454A")) +
+      #Getting proper datetime axis and dodging the tick labels
       scale_x_datetime(breaks = date_breaks("3 hour"), labels = date_format("%a %I:%M",
                                                                             tz = "EST"),
                        guide = guide_axis(n.dodge=2)) +
       scale_y_continuous(breaks = seq(0,100,by = 10), labels = seq(0,100,by=10)) +
-      # coord_cartesian(xlim = c(as.POSIXct("2020-11-03 18:00:00 EST"),
-      #                          as.POSIXct("2020-11-04 4:00:00 EST"))) +
       coord_cartesian(ylim=c(0,100)) +
       labs(title="How win probability has changed over time",
            subtitle = paste0("Updated every 2 minutes, may differ temporarily from needle. Last updated: ", t_string)) +
